540182e766937b347d03050e0ecfb36313547da9
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-02-05 23:52:41 +0530

    add Regions controller , made its controller to get all db context and assinged it to privet variable then return only regions from dbcontext

---
+++ DotNet-Training/Controllers/RegionsController.cs
@@ -0,0 +1,26 @@
+﻿using DotNet_Training.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet_Training.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly dasunDbcontext dbContext;
+
+        public RegionsController(dasunDbcontext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Getall()
+        {
+            var regionz = dbContext.Region.ToList();
+            return Ok(regionz);
+        }
+    }
+}
