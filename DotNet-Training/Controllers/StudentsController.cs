db591add6de3047626170f1dbf756582b7266392
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-02-05 21:37:11 +0530

    Student controller added

---
+++ DotNet-Training/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet_Training.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentnames = new string[] { "dsa", "thee", "kamal", "nelum" };
+            return Ok(studentnames);
+        }
+    }
+}
