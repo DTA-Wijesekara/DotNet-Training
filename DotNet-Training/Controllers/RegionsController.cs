ec845f86746fd178a51ed0e39293d68c705cbcd5
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-02-06 00:19:10 +0530

    added get by id action method to controller

--- DotNet-Training/Controllers/RegionsController.cs
+++ DotNet-Training/Controllers/RegionsController.cs
@@ -15,11 +15,26 @@ namespace DotNet_Training.Controllers
             this.dbContext = dbContext;
         }
 
-
+        //get all regios
+        //GET: https://localhost:portnumber/api/regions
         [HttpGet]
         public IActionResult Getall()
         {
             var regionz = dbContext.Region.ToList();
+            return Ok(regionz);
+        }
+
+        //get by ID
+        //GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")] 
+        public IActionResult Getbyid([FromRoute]Guid id)
+        {
+            var regionz = dbContext.Region.Find(id);
+            if(regionz == null)
+            {
+                return NotFound();
+            }
             return Ok(regionz);
         }
     }
