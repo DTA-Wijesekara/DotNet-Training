07c61df5675547c5b6b07d0d9de8211193587197
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-02-05 21:56:34 +0530

    added dasunDbContext

---
+++ DotNet-Training/Context/dasunDbcontext.cs
@@ -0,0 +1,16 @@
+﻿using DotNet_Training.Models.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet_Training.Context
+{
+    public class dasunDbcontext : DbContext
+    {
+        public dasunDbcontext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Region { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
