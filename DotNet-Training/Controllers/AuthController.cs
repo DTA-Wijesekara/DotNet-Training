5e30aaae80f7bc6f9d56239c029d63a2828fddb6
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-02-17 23:53:25 +0530

    Create Auth controller and Register Action method

---
+++ DotNet-Training/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using DotNet_Training.Models.DTO.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet_Training.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityuser = new IdentityUser
+            {
+                UserName = registerRequestDto.UserName,
+                Email = registerRequestDto.UserName
+            };
+            var identityResult = await userManager.CreateAsync(identityuser , registerRequestDto.Password);
+            if (identityResult.Succeeded)
+            {
+                if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityuser, registerRequestDto.Roles);
+                    if(identityResult.Succeeded)
+                    {
+                        return Ok("User registered success! please login");
+                    }
+                }
+            }
+            return BadRequest("Something went Wrong");
+        }
+    }
+}
